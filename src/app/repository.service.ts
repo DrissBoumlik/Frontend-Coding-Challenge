14ac5bc55db5864cdcadcb8424e45f2582502a58
Driss Boumlik <boumlik.t@gmail.com>
2019-04-28 19:42:43 +0100

    Change response type in getRepos method
    Response type was changed to 'any' to prevent the error of (Property 'items' does not exist ...)
    Same thing for the formatDate() method : extra field was added for the month to prevent any error of casting

--- src/app/repository.service.ts
+++ src/app/repository.service.ts
@@ -9,29 +9,33 @@ export class RepositoryService  {
   private repositories : Repository[] = [];
 
       constructor(private http: HttpClient) {}
+
       getRepos() {
-        var date = new Date();
-        let last_30_days = new Date(new Date(date.setDate(date.getDate() - 30)).toLocaleDateString().replace(/\//g,'-'));
-        last_30_days = this.formatDate(last_30_days);
+        this.repositories = []; // empty the repositories each time the request is sent
+        let last_30_days = this.formatDate(); // formatting the date to get yyyy-mm-dd format
         this.http.get('https://api.github.com/search/repositories?q=created:>' + last_30_days + '&sort=stars&order=desc')
-        .subscribe(data => {
+        .subscribe((data : any) => {
           for(let repo of data.items){
+            // pushing the repositories the the repositories[] array
             this.repositories.push(new Repository(
               repo.owner.login, repo.owner.avatar_url,
               repo.name, repo.description,
               repo.stargazers_count, repo.open_issues_count,
+              // calculating the difference between now and the repository submission date to get the total in days
               Math.ceil(Math.abs((new Date().getTime() - new Date(repo.updated_at).getTime()) / (1000 * 3600 * 24))) + ""));
             }
           });
           return this.repositories;
         }
 
-        formatDate(date) {
-          let year = date.getFullYear();
-          let month = date.getMonth() + 1
-          month = month < 10 ? '0' + month : month;
-          let day = date.getDate();
-          date = year + '-' + month + '-' + day;
-          return date;
+        formatDate() { //
+          let date = new Date();
+          date = new Date(new Date(date.setDate(date.getDate() - 30)).toLocaleDateString().replace(/\//g,'-'));
+          var year = date.getFullYear();
+          var _month = date.getMonth() + 1;
+          var month = _month < 10 ? '0' + _month : _month;
+          var day = date.getDate();
+          var last_30_days = year + '-' + month + '-' + day;
+          return last_30_days;
         }
       }
