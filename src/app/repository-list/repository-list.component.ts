0add6fd4ca4f4242cb2e1616a5935575c6afbd42
Driss Boumlik <boumlik.t@gmail.com>
2019-04-28 19:13:30 +0100

    Add property to prevent multiple clicks on the load button

--- src/app/repository-list/repository-list.component.ts
+++ src/app/repository-list/repository-list.component.ts
@@ -10,13 +10,17 @@ import { Repository } from '../Repository.model';
 })
 export class RepositoryListComponent implements OnInit {
   repositories: Repository[];
+  btn_load_active = true; // preventing user from clicking multiple times on the button
   constructor(private repositoryService: RepositoryService) { }
 
   ngOnInit() {
 
   }
   onLoad() {
-    this.repositories = this.repositoryService.getRepos();
-    // this.repositoryChanged.next(this.repositories)
+    this.btn_load_active = false; // disabling the load button after the first click
+    this.repositories = this.repositoryService.getRepos(); // getting the repositories from the repos service
+    setTimeout(() => {
+      this.btn_load_active = true; // enabling the load button after 3 seconds
+    }, 2000);
   }
 }
