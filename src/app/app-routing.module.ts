2a622540090da0becb78ca1305957c63deec189f
Driss Boumlik <boumlik.t@gmail.com>
2019-04-28 17:51:38 +0100

    Add route to app-routing module to load the repository list

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,10 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { RepositoryListComponent } from './repository-list/repository-list.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: RepositoryListComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
