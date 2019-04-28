2d149d61708d60b3b7b056b0b12a2ba05e2bc084
Driss Boumlik <boumlik.t@gmail.com>
2019-04-28 20:40:55 +0100

    Clear the repository array in a separate method clearRepos()

--- src/app/repository.service.ts
+++ src/app/repository.service.ts
@@ -8,34 +8,37 @@ export class RepositoryService  {
 
   private repositories : Repository[] = [];
 
-      constructor(private http: HttpClient) {}
+  constructor(private http: HttpClient) {}
 
-      getRepos(page) {
-        this.repositories = []; // empty the repositories each time the request is sent
-        let last_30_days = this.formatDate(); // formatting the date to get yyyy-mm-dd format
-        this.http.get('https://api.github.com/search/repositories?q=created:>' + last_30_days + '&sort=stars&order=desc&page=' + page)
-        .subscribe((data : any) => {
-          for(let repo of data.items){
-            // pushing the repositories the the repositories[] array
-            this.repositories.push(new Repository(
-              repo.owner.login, repo.owner.avatar_url,
-              repo.name, repo.description,
-              repo.stargazers_count, repo.open_issues_count,
-              // calculating the difference between now and the repository submission date to get the total in days
-              Math.ceil(Math.abs((new Date().getTime() - new Date(repo.updated_at).getTime()) / (1000 * 3600 * 24))) + ""));
-            }
-          });
-          return this.repositories;
+  getRepos(page) {
+    let last_30_days = this.formatDate(); // formatting the date to get yyyy-mm-dd format
+    this.http.get('https://api.github.com/search/repositories?q=created:>' + last_30_days + '&sort=stars&order=desc&page=' + page)
+    .subscribe((data : any) => {
+      for(let repo of data.items){
+        // pushing the repositories the the repositories[] array
+        this.repositories.push(new Repository(
+          repo.owner.login, repo.owner.avatar_url,
+          repo.name, repo.description,
+          repo.stargazers_count, repo.open_issues_count,
+          // calculating the difference between now and the repository submission date to get the total in days
+          Math.ceil(Math.abs((new Date().getTime() - new Date(repo.updated_at).getTime()) / (1000 * 3600 * 24))) + ""));
         }
+      });
+      return this.repositories;
+    }
 
-        formatDate() { //
-          let date = new Date();
-          date = new Date(new Date(date.setDate(date.getDate() - 30)).toLocaleDateString().replace(/\//g,'-'));
-          var year = date.getFullYear();
-          var _month = date.getMonth() + 1;
-          var month = _month < 10 ? '0' + _month : _month;
-          var day = date.getDate();
-          var last_30_days = year + '-' + month + '-' + day;
-          return last_30_days;
-        }
-      }
+    clearRepos() {
+      this.repositories = []; // empty the repositories each time the request is sent
+    }
+
+    formatDate() { //
+      let date = new Date();
+      date = new Date(new Date(date.setDate(date.getDate() - 30)).toLocaleDateString().replace(/\//g,'-'));
+      var year = date.getFullYear();
+      var _month = date.getMonth() + 1;
+      var month = _month < 10 ? '0' + _month : _month;
+      var day = date.getDate();
+      var last_30_days = year + '-' + month + '-' + day;
+      return last_30_days;
+    }
+  }
