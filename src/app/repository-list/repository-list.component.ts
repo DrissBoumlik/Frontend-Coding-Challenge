8f047e6ceaa72eea7c3ab1d98f2d18211d74ef34
Driss Boumlik <boumlik.t@gmail.com>
2019-04-28 21:03:32 +0100

    Load the repositories when the page loads
    The repositories are loaded when the page loads instead of the button click

--- src/app/repository-list/repository-list.component.ts
+++ src/app/repository-list/repository-list.component.ts
@@ -10,22 +10,13 @@ import { Repository } from '../Repository.model';
 })
 export class RepositoryListComponent implements OnInit {
   repositories: Repository[];
-  btn_load_active = true; // preventing user from clicking multiple times on the button
   page = 2; // serves as page number for the infinit scrolling
   constructor(private repositoryService: RepositoryService) { }
 
   ngOnInit() {
-
-  }
-  onLoad() {
-    this.btn_load_active = false; // disabling the load button after the first click
     this.repositoryService.clearRepos();
     this.repositories = this.repositoryService.getRepos(1); // getting the repositories from the repos service
-    setTimeout(() => {
-      this.btn_load_active = true; // enabling the load button after 3 seconds
-    }, 2000);
   }
-
   onScroll() {
     this.repositories = this.repositoryService.getRepos(this.page++);
   }
