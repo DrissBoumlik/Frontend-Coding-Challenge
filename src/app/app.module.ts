c68f9197a1353525131f31cadc160d641d566dec
Driss Boumlik <boumlik.t@gmail.com>
2019-04-28 17:49:01 +0100

    Add missing declarations and imports to the app.module
    The Missing items :
    Declarations :
      RepositoryList
      RepositoryItem
    Imports :
      HttpClientModule

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,13 +3,19 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { RepositoryListComponent } from './repository-list/repository-list.component';
+import { RepositoryItemComponent } from './Repository-List/repository-item/repository-item.component';
+import { HttpClientModule } from '@angular/common/http';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    RepositoryListComponent,
+    RepositoryItemComponent
   ],
   imports: [
     BrowserModule,
+    HttpClientModule,
     AppRoutingModule
   ],
   providers: [],
